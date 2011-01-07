4027270e02a81dcc229ede9830116238986e3a1e
Adam Dawkins <adam@Adam-Dawkinss-MacBook-Pro.local>
2011-01-07 09:34:20 +0000

    remove link from blockqutoes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	var $quotes = $(".quote", $("#main"));
-	$quotes.find("blockquote").click(function(){
+	/* $quotes.find("blockquote").click(function(){
 		window.location = "/quotes/"+$(this).parent().attr("id").split("_")[1];
-	});
+	}); */
 	$quotes.ligatures();
 });
